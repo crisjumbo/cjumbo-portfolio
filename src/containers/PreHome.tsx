c61bca5dbaa9a8aedbd8a26699506c42284b7d1b
aybike <tonn.raus09@gmail.com>
2021-01-10 15:39:39 +0100

    package(helmet): added for seo

--- src/containers/PreHome.tsx
+++ src/containers/PreHome.tsx
@@ -8,25 +8,28 @@
 */
 import React from 'react';
 import { SMain, SWrapper, SImg, SLink } from '../styled/containers/PreHome';
-// const personalLogo = require('../assets/statics/logoChr.png');
-// import PersonalLogo from '../assets/statics/logoChr.png';
-
+import { Helmet } from 'react-helmet';
 //-- PreHome Component
 const PreHome: React.FC = () => {
   //-- Render of the component
   return (
-    <SMain>
-      <SWrapper>
-        <SLink to="/projects">
-          <SImg src={'https://i.ibb.co/1ZhmVH5/icon.png'} alt="Personal Logo" />
-        </SLink>
-        <p>
-          Helloª, I am Cristofher a web developer.
-          <br />
-          As goal, the future. As language, technology.
-        </p>
-      </SWrapper>
-    </SMain>
+    <>
+      <Helmet>
+        <title>Chr.Jimenez - Home</title>
+      </Helmet>
+      <SMain>
+        <SWrapper>
+          <SLink to="/projects">
+            <SImg src={'https://i.ibb.co/1ZhmVH5/icon.png'} alt="Personal Logo" />
+          </SLink>
+          <p>
+            Helloª, I am Cristofher a web developer.
+            <br />
+            As goal, the future. As language, technology.
+          </p>
+        </SWrapper>
+      </SMain>
+    </>
   );
 };
 
