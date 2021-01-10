fd90167bc86b5a2018977893fdd2cd8e7445674c
aybike <tonn.raus09@gmail.com>
2021-01-10 16:27:28 +0100

    doc(env): added

--- src/containers/PreHome.tsx
+++ src/containers/PreHome.tsx
@@ -9,6 +9,8 @@
 import React from 'react';
 import { SMain, SWrapper, SImg, SLink } from '../styled/containers/PreHome';
 import { Helmet } from 'react-helmet';
+
+const FB_ID: string | undefined = process.env.ID_APP_FACEBOOK;
 //-- PreHome Component
 const PreHome: React.FC = () => {
   //-- Render of the component
@@ -16,6 +18,21 @@ const PreHome: React.FC = () => {
     <>
       <Helmet>
         <title>Chr.Jimenez - Home</title>
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@Ayton_que" />
+        <meta name="twitter:creator" content="@Ayton_que" />
+        <meta name="twitter:title" content="Chr Jimenez" />
+        <meta name="twitter:description" content="Cristofher Jimenez Portfolio" />
+        <meta name="twitter:image" content="https://i.ibb.co/1RQy2kV/card-chr-jimenez.png" />
+        <meta property="og:title" content="Chr Jimenez" />
+        <meta property="og:description" content="Cristofher Jimenez Portfolio" />
+        <meta property="og:image" content="https://i.ibb.co/1RQy2kV/card-chr-jimenez.png" />
+        <meta property="og:url" content="chr-jimenez.com" />
+        <meta property="og:site_name" content="Chr jimenez" />
+        <meta property="og:locale" content="es_ES" />
+        <meta property="og:type" content="article" />
+        <meta property="fb:app_id" content={FB_ID} />
       </Helmet>
       <SMain>
         <SWrapper>
