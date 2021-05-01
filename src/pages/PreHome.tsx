bfaf7dbd3cbba9b2f53da55d1e8c0ac4472b85b9
aybike <tonn.raus09@gmail.com>
2021-05-01 17:02:11 +0200

    bug: dont repeat yourself

--- src/pages/PreHome.tsx
+++ src/pages/PreHome.tsx
@@ -9,7 +9,6 @@
 import React from 'react';
 import { SMain, SWrapper, SImg, SLink } from '../styled/containers/PreHome';
 import { Helmet } from 'react-helmet';
-const FB_ID: string | undefined = 'needs be set in the backend';
 
 //-- PreHome Component
 const PreHome: React.FC = () => {
@@ -18,21 +17,6 @@ const PreHome: React.FC = () => {
     <>
       <Helmet>
         <title>Chr.jimenez</title>
-
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@Ayton_que" />
-        <meta name="twitter:creator" content="@Ayton_que" />
-        <meta name="twitter:title" content="Chr Jimenez" />
-        <meta name="twitter:description" content="Cristofher Jimenez Portfolio" />
-        <meta name="twitter:image" content="https://i.ibb.co/1RQy2kV/card-chr-jimenez.png" />
-        <meta property="og:title" content="Chr Jimenez" />
-        <meta property="og:description" content="Cristofher Jimenez Portfolio" />
-        <meta property="og:image" content="https://i.ibb.co/1RQy2kV/card-chr-jimenez.png" />
-        <meta property="og:url" content="chr-jimenez.com" />
-        <meta property="og:site_name" content="Chr jimenez" />
-        <meta property="og:locale" content="es_ES" />
-        <meta property="og:type" content="article" />
-        <meta property="fb:app_id" content={FB_ID} />
       </Helmet>
       <SMain>
         <SWrapper>
