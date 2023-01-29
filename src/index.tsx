3df6de6828e1fa1206b80182e8a3122f48c81750
Cristofher Jumbo jiménez <cristofherjumbojimenez@Cristofhers-MacBook-Pro-2.local>
2023-01-29 20:03:46 +0100

    remove all files apart from cname and readme

--- src/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './routes/App';
-
-const container = document.getElementById('App');
-
-ReactDOM.render(<App></App>, container);
